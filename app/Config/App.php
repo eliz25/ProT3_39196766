623b7aa0cf49f7ed517e41eb98b1a9d927d742d9
Yeny <yenyvelaz@gmail.com>
2025-06-25 23:35:27 -0300

    cambio localhost

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/yeny';
+    public string $baseURL = 'http://localhost/ProT3_39196766';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
